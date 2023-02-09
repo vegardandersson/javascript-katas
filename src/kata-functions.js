5308914e651e035774a7dbec9cab222952a0e803
vegardandersson <vegardandersson@outlook.com>
2023-02-09 08:41:36 +0100

    kata to check if a number exists in an array that is larger than input

--- src/kata-functions.js
+++ src/kata-functions.js
@@ -1,5 +1,15 @@
 
 
-function kataButtonClick() {
-    console.log("hello");
+function existsHigher(numbers, num){
+    let existsHigher = false;
+    let index = 0;
+    while(!existsHigher && index < numbers.length){
+        existsHigher = (numbers[index] > num);
+        index += 1;
+    }
+    return existsHigher;
 }
+
+console.log(existsHigher([1, 2, 5, 6], 9));
+console.log(existsHigher([0, 3, 5], 2));
+console.log(existsHigher([], 6));
