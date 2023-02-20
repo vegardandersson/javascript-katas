256254afc1734e55743479c52fa8ffd04dea1fe2
vegardandersson <vegardandersson@outlook.com>
2023-02-20 08:53:09 +0100

    kata to find boomerangs in number arrays

--- src/kata-functions.js
+++ src/kata-functions.js
@@ -10,6 +10,42 @@ function existsHigher(numbers, num){
     return existsHigher;
 }
 
-console.log(existsHigher([1, 2, 5, 6], 9));
-console.log(existsHigher([0, 3, 5], 2));
-console.log(existsHigher([], 6));
+//console.log(existsHigher([1, 2, 5, 6], 9));
+//console.log(existsHigher([0, 3, 5], 2));
+//console.log(existsHigher([], 6));
+
+
+function toPigLatin(input) {
+    const vowels = (/^[aeiou]$/i);
+    return input.split(" ").map(element => {
+        if(element[0].match(vowels)){
+            return element += "way";
+        }else{
+            let vowel = false;
+            let movedCharacters = "";
+            while(!vowel){
+                if(!element[0].match(vowels)){
+                    movedCharacters += element[0];
+                    element = element.substring(1);
+                }else{
+                    vowel = true;
+                }
+            }
+            return element + movedCharacters + "ay";
+        }
+    }).join(" ");
+}
+
+function findBoomerangs(input) {
+    let result  = [];
+    for(let i = 0; i < input.length-2; i++){
+        let temp = input.slice(i, i+3);
+        if(temp[0] == temp[2] && temp[1] != temp[0]){
+            result.push(temp)
+        }
+    }
+    return result;
+}
+
+console.log(findBoomerangs([3, 7, 3, 2, 1, 5, 1, 2, 2, -2, 2]))
+console.log(findBoomerangs([1, 7, 1, 7, 1, 7, 1]))
